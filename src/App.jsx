36894c49608e6b8275ffafa06e770c6b7a648d74
dbs94150-alt <dbs94150@gmail.com>
2025-12-16 09:33:03 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
 function App() {
   return (
-    <div style={{ color: "white", background: "#1e1f22", height: "100vh" }}>
-      Discord Clone
+    <div
+      style={{
+        display: "flex",
+        height: "100vh",
+        backgroundColor: "#1e1f22",
+        color: "white",
+      }}
+    >
+      {/* Servers */}
+      <div style={{ width: 72, backgroundColor: "#1e1f22" }}>
+        SERVERS
+      </div>
+
+      {/* Channels */}
+      <div style={{ width: 240, backgroundColor: "#2b2d31" }}>
+        CHANNELS
+      </div>
+
+      {/* Chat */}
+      <div style={{ flex: 1, backgroundColor: "#313338" }}>
+        CHAT
+      </div>
     </div>
   );
 }
